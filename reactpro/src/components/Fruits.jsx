5436aed7eeddfe8ee43d324f520e37459bdfebc5
MahabirGupta <mahabirg81@gmail.com>
2025-01-07 20:50:41 +0800

    render an array object in a react component

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,13 +1,21 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Pear", "Oranges", "Bananas", "Water-melon"];
+  // const fruits = ["Apple", "Pear", "Oranges", "Bananas", "Water-melon"];
 
-  // create
-  // const fruits = [];
+  // create an object array
+  const fruits = [
+    { name: "Apple", price: 2.6, emoji: "🍎" },
+    { name: "Pear", price: 3.65, emoji: "🍐" },
+    { name: "Oranges", price: 0.6, emoji: "🍊" },
+    { name: "Bananas", price: 3.3, emoji: "🍌" },
+    { name: "Water-melon", price: 4.2, emoji: "🍉" },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.name}>
+            {fruit.emoji} {fruit.name} ${fruit.price}
+          </li>
         ))}
       </ul>
     </div>
