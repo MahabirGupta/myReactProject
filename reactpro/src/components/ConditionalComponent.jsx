131a70ca33627282e044e99b825355fb2e8d9e45
MahabirGupta <mahabirg81@gmail.com>
2025-01-07 21:44:34 +0800

    Conditional rendering JSX & Components

--- reactpro/src/components/ConditionalComponent.jsx
+++ reactpro/src/components/ConditionalComponent.jsx
@@ -1,16 +1,24 @@
+import Welcome from "./Welcome";
+import Code from "./Code";
+
 export default function ConditionalComponent() {
-  const display = false;
+  const display = true;
+  // if (display) {
+  //   return (
+  //     <div>
+  //       <h3>This is a Conditional Component</h3>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       <h3>Code everyday!</h3>
+  //     </div>
+  //   );
+  // }
   if (display) {
-    return (
-      <div>
-        <h3>This is a Conditional Component</h3>
-      </div>
-    );
+    return <Welcome />;
   } else {
-    return (
-      <div>
-        <h3>Code everyday!</h3>
-      </div>
-    );
+    return <Code />;
   }
 }
