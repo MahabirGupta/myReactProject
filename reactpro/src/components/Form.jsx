399b632e1638ae076ece4828caea48b73fbe98a8
MahabirGupta <mahabirg81@gmail.com>
2025-01-13 12:29:36 +0800

    handing multiple inputs in react

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState(""); // initial value of name to be empty
+  const [name, setName] = useState({ firstName: "", lastName: "" }); // create firstName and lastName objects
   // function handleChange(e) {
   //   setName(e.target.value);
   // }
   return (
     <div>
+      {name.firstName} - {name.lastName}
       <form>
         {/* pass in the event,e in the demo function */}
         <input
@@ -14,9 +15,16 @@ export default function Form() {
           //   return handleChange(e);
           // }}
           // onChange={(e) => handleChange(e)}
-          onChange={(e) => setName(e.target.value)}
+          // spread operator ...name
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstName}
+        />
+        <input
+          type="text"
+          // spread operator ...name to prevent the first name from being overwritten
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          value={name.lastName}
         />
       </form>
     </div>
